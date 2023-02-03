f4559c2167ff7042b07960958a79121976a2e446
Claudineijoseprill <32801503+Claudineijoseprill@users.noreply.github.com>
2023-02-02 22:40:37 -0300

    Create ejemplo.c

---
+++ ejemplo.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+#include "str.h"
+#include "str.c"
+
+int main(){
+
+    str_t *texto = str_new("Inicio\n");
+    for(int i=0; texto->len<500000; i++){
+        str_cat(texto, "esta es la linea ");
+        str_cat_int(texto, i);
+        str_cat_char(texto, '\n');
+    }
+
+    printf("texto %s tamanho %lld\n", texto->c, texto->len);
+
+    sleep(2);
+    printf("freeee\n");
+    str_free(texto);   // nunca se olviden de limpiar la variable creada con str_new
+    sleep(2);
+
+    return 0;
+}
