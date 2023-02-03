5b5b95bdfb4eb597e9f29c0a35e84ec6fb13bf1a
Claudineijoseprill <32801503+Claudineijoseprill@users.noreply.github.com>
2023-02-02 22:33:13 -0300

    Update str.c

--- str.c
+++ str.c
@@ -1,7 +1,7 @@
 
 str_t *str_new(char const *str){
     str_t *re = (str_t*)calloc(sizeof(str_t), 1);
-    int ss = re->size = STR_DEFAULT_SIZE;
+    re->size = STR_DEFAULT_SIZE;
     re->c = (char*)calloc(sizeof(char), re->size);
     re->len = 0;
 
@@ -20,12 +20,12 @@ str_t *str_cat_str(str_t *d, str_t const *da){
     return d;
 }
 str_t *str_cat(str_t *d, char const *da){
-    if(d == NULL || da == NULL) return;
+    if(d == NULL || da == NULL) return d;
 
-    int i, e, s=d->size-1;
+    lo_t i, e, s=d->size-1;
     for(i=d->len, e=0; da[e]!=0; i++,e++){
         if(i == s){
-            d->size += (d->size*50)/100;// mas 50%
+            d->size += (d->size*10)/100;// mas 10%
             s = d->size-1;
 
             char *tmp = (char*)calloc(sizeof(char), d->size);
@@ -40,7 +40,7 @@ str_t *str_cat(str_t *d, char const *da){
     d->c[i] = 0;
     return d;
 }
-str_t *str_cat_int_a(str_t *d, long long const da, int apartir){
+str_t *str_cat_int_a(str_t *d, lo_t const da, lo_t apartir){
     if(apartir < d->len){
 
         d->len = apartir;
@@ -51,13 +51,13 @@ str_t *str_cat_int_a(str_t *d, long long const da, int apartir){
     str_cat_int(d, da);
     return d;
 }
-str_t *str_cat_n(str_t *d, char const *da, int n){
-    if(d == NULL || da == NULL) return;
-    int i, e, s=d->size-1;
+str_t *str_cat_n(str_t *d, char const *da, lo_t n){
+    if(d == NULL || da == NULL) return d;
+    lo_t i, e, s=d->size-1;
 
     for(i=d->len, e=0; e<n&&da[e]!=0; i++,e++){
         if(i == s){
-            d->size += (d->size*50)/100;// mas 50%
+            d->size += (d->size*10)/100;// mas 10%
             s = d->size-1;
 
             char *tmp = (char*)calloc(sizeof(char), d->size);
@@ -76,7 +76,7 @@ str_t *str_cat_char(str_t *d, char da){
     if(d != NULL && da != 0){
 
         if(d->len+1 >= d->size){
-            d->size += (d->size*50)/100;// mas 50%
+            d->size += (d->size*10)/100;// mas 10%
             
             char *tmp = (char*)calloc(sizeof(char), d->size);
 
@@ -90,11 +90,12 @@ str_t *str_cat_char(str_t *d, char da){
     }
     return d;
 }
-void caSet(str_t *d, char const *da){
-    caCle(d);
-    caAdd(d, da);
+str_t *str_set(str_t *d, char const *da){
+    str_clear(d);
+    str_cat(d, da);
+    return d;
 }
-str_t *str_cat_int(ca_t *d, long long da){
+str_t *str_cat_int(str_t *d, lo_t da){
     if(d->size - d->len < 128){
 
         d->size += 128;
@@ -111,7 +112,7 @@ str_t *str_cat_int(ca_t *d, long long da){
   
     return d;
 }
-str_t *str_cat_bol(str_t *d, u_char da){
+str_t *str_cat_bol(str_t *d, unsigned char da){
     if(da) str_cat(d, "true");
     else str_cat(d, "false");
   
@@ -134,7 +135,7 @@ str_t *str_cat_dou(str_t *d, double da){
   
     return d;
 }
-str_t *str_n_del(str_t *d, u_short del){
+str_t *str_n_del(str_t *d, lo_t del){
     d->len -= del;
     d->c[d->len] = 0;
   
@@ -148,9 +149,9 @@ str_t *str_free(str_t *d){
     d = NULL;
     return d;
 }
-int *str_fnd(str_t *ca, char const *fn){
-    int static re[100] = {-1};
-    int i, e,
+lo_t *str_fnd(str_t *ca, char const *fn){
+    lo_t static re[100] = {-1};
+    lo_t i, e,
     pos = 1,
     slen = strlen(fn);
     for(e=0; e<ca->len; e++){
@@ -168,8 +169,8 @@ int *str_fnd(str_t *ca, char const *fn){
 str_t *str_set_size(str_t *d, size_t size){
     if(d != NULL && size > d->len){
 
-        int i;
-        d->tm = u_ch(size);
+        lo_t i;
+        d->tm = (char*)calloc(sizeof(char), size);
         for(i=0; i<d->len; i++) d->tm[i] = d->c[i];
         d->tm[i] = 0;
 
@@ -186,7 +187,7 @@ str_t *str_clear(str_t *d){
 }
 str_t *str_clon(str_t const *d){
     if(d){
-        str_t *re = caNewS(d->len+10);
+        str_t *re = str_new_size(d->len+10);
         strcpy(re->c, d->c);
         re->len = d->len;
         return re;
